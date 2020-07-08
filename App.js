9c80d26824b80ee862bb4ca49a932a2921dd9ee2
tejas6083 <tejas6083@gmail.com>
2020-07-08 19:45:30 +0530

    Login, Welcome, Register Included

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Index from "./source/views/index";
+import { View, StyleSheet, ScrollView, Text } from "react-native";
+
+import { Provider } from "react-redux";
+import store from "./source/redux/store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Index />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
